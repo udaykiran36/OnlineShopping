fc1717dfdaf07512f9b68d86830b2fa3434b6e98
udayk <udayk@DESKTOP-2QILD73>
2017-05-12 17:47:56 +0530

    commit of backend

---
+++ backend/src/main/java/com/niit/backend/daoimp/SupplierDAOImpl.java
@@ -0,0 +1,89 @@
+package com.niit.backend.daoimp;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.niit.backend.dao.SupplierDAO;
+import com.niit.backend.domain.Supplier;
+
+@Repository("supplierDAO")
+@Transactional
+public class SupplierDAOImpl implements SupplierDAO
+{
+	@Autowired
+	private SessionFactory sessionFactory;
+	public SupplierDAOImpl(SessionFactory sessionFactory)
+	{
+		this.sessionFactory=sessionFactory;
+	}
+	@Override
+	public List<Supplier> list()
+	{
+		return sessionFactory.getCurrentSession().createQuery("from category").list();
+	}
+
+	@Override
+	public boolean save(Supplier supplier)
+	{
+		try
+		{
+			sessionFactory.getCurrentSession().save(supplier);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	
+	}
+
+	@Override
+	public boolean update(Supplier supplier)
+	{
+
+		try
+		{
+			sessionFactory.getCurrentSession().update(supplier);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean delete(String id)
+	{
+		try
+		{
+			sessionFactory.getCurrentSession().delete(getSupplierByID(id));
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public Supplier getSupplierByID(String id)
+	{
+		return (Supplier) sessionFactory.getCurrentSession().get(Supplier.class, id);
+	}
+
+	@Override
+	public Supplier getSupplierByName(String name)
+	{
+		return  (Supplier) sessionFactory.getCurrentSession().createQuery("from supplier where name=?").setString(0,name).uniqueResult();
+	}
+
+}
