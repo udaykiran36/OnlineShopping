82fedfb30fa585876fe46066bb8c50c376c021f5
udayk <udayk@DESKTOP-2QILD73>
2017-05-12 18:23:52 +0530

    commit of backend

---
+++ backend/src/main/java/com/niit/backend/dao/ProductDAO.java
@@ -0,0 +1,15 @@
+package com.niit.backend.dao;
+
+import java.util.List;
+
+import com.niit.backend.domain.Product;
+
+public interface ProductDAO
+{
+	public List<Product> list();
+	public boolean save(Product product);
+	public boolean update(Product product);
+	public boolean delete(String id);
+	public Product getProductByID(String id);
+	public Product getProductByName(String name);
+}
