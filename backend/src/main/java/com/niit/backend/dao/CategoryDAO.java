8629f5005f3eece5a80368c5d2dec307150a2bb4
udayk <udayk@DESKTOP-2QILD73>
2017-05-12 16:48:52 +0530

    commit of backend

---
+++ backend/src/main/java/com/niit/backend/dao/CategoryDAO.java
@@ -0,0 +1,15 @@
+package com.niit.backend.dao;
+
+import java.util.List;
+
+import com.niit.backend.domain.Category;
+
+public interface CategoryDAO
+{
+	public List<Category> list();
+	public boolean save(Category category);
+	public boolean update(Category category);
+	public boolean delete(String id);
+	public Category getCategoryByID(String id);
+	public Category getCategoryByName(String name);
+}
