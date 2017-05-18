3e8c3715dfe55f029de986d5731975c495e86ba4
udayk <udayk@DESKTOP-2QILD73>
2017-05-18 17:32:47 +0530

    commit of forntend

--- forntendproject/src/main/java/com/niit/forntendproject/controller/HomeController.java
+++ forntendproject/src/main/java/com/niit/forntendproject/controller/HomeController.java
@@ -4,18 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 public class HomeController
 {
-	
 	@RequestMapping( value ={"/","/home","/index" })
 	public ModelAndView home()
 	{
 		 ModelAndView mv=new ModelAndView("home");
 		 return mv;
-		 
 	}
-	
 	@RequestMapping( value ={"/login"})
 	public ModelAndView myLoginPage()
 	{	
