854ffa37ff3ecff5554040179b7ff2601adec1ff
udayk <udayk@UdayKiran>
2017-05-21 16:55:41 +0530

    commit of frontend

--- forntendproject/src/main/webapp/assests/js/CustomJS.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(e) {
-	//Changing padding dynamically to match the navbar height
-	var h = $('nav').height() + 20;
-	
-	
-	$('body').animate({
-		paddingTop : h
-	});
-});
